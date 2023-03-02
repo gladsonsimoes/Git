e1d0a74e9f6a6a67e02d697a40bd2a8ac5721eed
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-03-02 20:21:07 -0300

    Update Remover_User_Git.bat

--- Programs/Remover_User_Git.bat
+++ Programs/Remover_User_Git.bat
@@ -1,3 +1,23 @@
+echo off
+
+color a 
+
 git config --global --unset user.name
 
 git config --global --unset user.email
+
+cls 
+
+echo =============================
+echo Usuario Removido com Sucesso!  
+echo =============================
+echo.
+echo.
+echo Confira se foi:
+echo.
+
+git config --list --global
+
+echo.
+echo.
+pause
