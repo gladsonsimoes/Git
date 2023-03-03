354a8fb4d2cb79f1dd23dd093a200e5958280383
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-03-03 20:39:51 -0300

    Add files via upload

---
+++ Programs/ver_usuario_git.bat
@@ -0,0 +1,10 @@
+echo off
+color a
+echo =====================================
+echo             Veja o Usuario 
+echo =====================================
+git config --list --global
+
+echo.
+echo.
+pause
